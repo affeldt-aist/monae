8326aab8ed0a3723acd68f5869266c261aa202eb
Takafumi Saikawa <tscompor@gmail.com>
2019-10-27 19:03:08 +0900

    wip on bindaltDl and bindchoiceDl

--- altprob_model.v
+++ altprob_model.v
@@ -12,7 +12,7 @@ Unset Printing Implicit Defensive.
 
 (* work in progress *)
 
-Require Import monad fail_monad proba_monad monad_model gcm_model.
+Require Import monae_lib monad fail_monad proba_monad monad_model gcm_model.
 
 Section P_delta_monad.
 End P_delta_monad.
@@ -30,9 +30,37 @@ Definition choice p A (x y : m A) : m A := x <|p|> y.
 
 Lemma altA A : associative (@alt A).
 Proof. by move=> x y z; rewrite /alt joetA. Qed.
+
+Section bindaltDl.
 Lemma bindaltDl : BindLaws.left_distributive (@Bind m) alt.
 Proof.
+move=> A B x y k.
+rewrite !bindE /alt.
+suff-> : (m # k) (x [+] y) = (m # k) x [+] (m # k) y.
+suff-> : forall T (u v : m (m T)), Join (u [+] v : m (m T)) = Join u [+] Join v
+    by done.
+move=> T u v.
+Import category.
+Import homcomp_notation.
+Local Notation F1 := free_semiCompSemiLattConvType.
+Local Notation F0 := free_convType.
+Local Notation FC := free_choiceType.
+Local Notation UC := forget_choiceType.
+Local Notation U0 := forget_convType.
+Local Notation U1 := forget_semiCompSemiLattConvType.
+rewrite /= /Monad_of_category_monad.join /=.
+rewrite HCompId HIdComp epsE'' !HCompId -!NIdFComp !HIdComp.
+have-> : (F1 \O F0) # epsC (U0 (U1 (P_delta_left T))) = idfun :> (_ -> _).
+  have -> : epsC (U0 (U1 (P_delta_left T))) =
+            [NEq _, _] _ by rewrite hom_ext /= epsCE.
+  by rewrite functor_id.
+rewrite compfid.
+rewrite compE.
+have-> : (F1 # eps0 (U1 (P_delta_left T))) (u [+] v) = 
+         (F1 # eps0 (U1 (P_delta_left T))) u [+]
+         (F1 # eps0 (U1 (P_delta_left T))) v.
 Admitted.
+End bindaltDl.
 
 Definition P_delta_monadAltMixin : MonadAlt.mixin_of m :=
   MonadAlt.Mixin altA bindaltDl.
@@ -73,8 +101,32 @@ case/boolP : (s == `Pr 0) => s0.
 rewrite /choice convA (@r_of_pq_is_r _ _ r s) //; congr ((_ <| _ |> _) <| _ |> _).
 by apply/prob_ext; rewrite s_of_pqE -H2 onemK.
 Qed.
+
+Section bindchoiceDl.
 Lemma bindchoiceDl p : BindLaws.left_distributive (@Bind m) (@choice p).
+move=> A B x y k.
+rewrite !bindE /choice.
+suff-> : (m # k) (x <|p|> y) = (m # k) x <|p|> (m # k) y.
+suff-> : forall T (u v : m (m T)), Join (u <|p|> v : m (m T)) = Join u <|p|> Join v
+    by done.
+move=> T u v.
+Import category.
+Import homcomp_notation.
+Local Notation F1 := free_semiCompSemiLattConvType.
+Local Notation F0 := free_convType.
+Local Notation FC := free_choiceType.
+Local Notation UC := forget_choiceType.
+Local Notation U0 := forget_convType.
+Local Notation U1 := forget_semiCompSemiLattConvType.
+rewrite /= /Monad_of_category_monad.join /=.
+rewrite HCompId HIdComp epsE'' !HCompId -!NIdFComp !HIdComp.
+have-> : (F1 \O F0) # epsC (U0 (U1 (P_delta_left T))) = idfun :> (_ -> _).
+  have -> : epsC (U0 (U1 (P_delta_left T))) =
+            [NEq _, _] _ by rewrite hom_ext /= epsCE.
+  by rewrite functor_id.
+rewrite compfid.
 Admitted.
+End bindchoiceDl.
 
 Definition P_delta_monadProbMixin : MonadProb.mixin_of m :=
   MonadProb.Mixin choice0 choice1 choiceC choicemm choiceA bindchoiceDl.
