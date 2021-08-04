95b97b730e0f92e863fd42b6f4fb7eb51fdb2cda
saito ayumu <awe.nun02@gmail.com>
2021-08-04 11:36:03 +0900

    tentative iqsort

--- example_array.v
+++ example_array.v
@@ -25,7 +25,7 @@ Section marray.
 (* Variables (E : UU0) (M : arrayMonad E Z_eqType).
 Variables (d : unit) (T : porderType d). *)
 
-Variable (d : unit) (E : porderType d) (M : arrayMonad E Z_eqType).
+Variable (d : unit) (E : porderType d) (M : plusArrayMonad E Z_eqType).
 
 Fixpoint readList (i : Z) (n : nat) : M (seq E) :=
   if n isn't k.+1 then Ret [::] else liftM2 cons (aget i) (readList (i + 1) k).
@@ -44,29 +44,37 @@ Definition write3L (i : Z) '(xs, ys, zs) :=
 Definition swap (i j : Z) : M unit := 
   aget i >>= (fun x => aget j >>= (fun y => aput i y >> aput j x)).
 
-Fixpoint partl (p : E) (s : (seq E * seq E)%type) (xs : seq E): (seq E * seq E)%type :=
+Fixpoint partl (p : E) (s : (seq E * seq E)%type) (xs : seq E)
+    : (seq E * seq E)%type :=
   match xs with
   | [::] => s
   | x :: xs => if x <= p then partl p (s.1 ++ [:: x], s.2) xs
                          else partl p (s.1, s.2 ++ [:: x]) xs
   end.
 
-Definition second {A B C} (f : B -> M C) (xy : (A * B)%type) := f xy.2 >>= (fun y' => Ret (xy.1, y')).
+Definition second {A B C} (f : B -> M C) (xy : (A * B)%type) := 
+  f xy.2 >>= (fun y' => Ret (xy.1, y')).
 
-(* Program Fixpoint partl' (p : E) (s : (seq E * seq E * seq E)) : M (seq E * seq E)%type :=
+(* Unset Guard Checking. *)
+(* Program Fixpoint partl' (p : E) (s : (seq E * seq E * seq E)) 
+    : M (seq E * seq E)%type :=
   match s with
   | (ys, zs, [::]) => Ret (ys, zs)
-  | (ys, zs, x :: xs) => if x <= p then (@qperm _ _ zs) >>= (fun zs' => partl' p (ys ++ [:: x], zs', xs))
-                         else partl' p (ys, zs ++ [:: x], xs)
+  | (ys, zs, x :: xs) => 
+    if x <= p then (@qperm _ _ zs) >>= 
+                (fun zs' => partl' p (ys ++ [:: x], zs', xs))
+              else partl' p (ys, zs ++ [:: x], xs)
   end. *)
 
 (* Definition dispatch (x p : E) '(ys, zs, xs) := 
   if x <= p then perm zs >>= (fun zs' => Ret (ys ++ [:: x], zs', xs))
             else perm (zs ++ [:: x]) >>= (fun zs' => Ret (ys, zs', zs)). *)
 
-(* Lemma ipartl_spec (i : Z) (p : E) (ys zs xs : seq E) (ipartl : E -> Z -> (nat * nat * nat)%type -> M (nat * nat)%type): 
+Lemma ipartl_spec (i : Z) (p : E) (ys zs xs : seq E) 
+    (ipartl : E -> Z -> (nat * nat * nat)%type -> M (nat * nat)%type) : 
   writeList i (ys ++ zs ++ xs) >> ipartl p i (size ys, size zs, size xs)
-    `<=` second perm (partl p (ys, zs) xs) >>= write2L i. *)
+  `<=` second perm (partl p (ys, zs) xs) >>= write2L i.
+Proof. Admitted.
 
 Local Open Scope zarith_ext_scope.
 
@@ -197,6 +205,32 @@ Fixpoint ipartl (p : E) (i : Z) (ny nz : nat) (nx : nat) : M (nat * nat)%type :=
            else
              ipartl p i ny nz.+1 k)
   end.
+
+Local Obligation Tactic := idtac.
+Program Fixpoint iqsort' (ni : (Z * nat))
+    (f : forall (n'j : (Z * nat)), (n'j.2 < ni.2)%coq_nat -> M unit) : M unit :=
+  match ni.2 with
+  | 0 => Ret tt
+  | n.+1 => aget ni.1 >>= (fun p =>
+            ipartl p (ni.1 + 1) 0 0 n >>= (fun '(ny, nz) =>
+            swap ni.1 (ni.1 + ny%:Z) >>
+            f (ni.1, ny) _  >> f ((ni.1 + ny%:Z + 1)%Z, nz) _))
+  end.
+Next Obligation.
+rewrite /=.
+move => [i [//|n]] /=.
+Admitted.
+Next Obligation.
+Admitted.
+
+Lemma well_founded_lt2 : well_founded (fun x y : (Z * nat) => (x.2 < y.2)%coq_nat).
+Proof. 
+  apply: (@well_founded_lt_compat _ _).
+  move => [x1 x2] [y1 y2]; exact.
+Qed.
+  
+Definition iqsort : (Z * nat) -> M unit :=
+  Fix well_founded_lt2 (fun _ => M unit) iqsort'.
 
 Fail Fixpoint iqsort (i : Z) (n : nat) : M unit :=
   match n with
