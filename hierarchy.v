cdf907d92b55efb24dc1b8c0336c97579bc5bd38
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2023-07-14 10:44:47 +0900

    make loc contents abstract in hierarchy.v

--- hierarchy.v
+++ hierarchy.v
@@ -1084,8 +1084,8 @@ HB.structure Definition MonadPlusArray (S : UU0) (I : eqType) :=
 Module Type MLTY.
 Parameter ml_type : Set.
 Parameter ml_type_eq_dec : forall x y : ml_type, {x=y}+{x<>y}.
-Variant loc : ml_type -> Type := mkloc T : nat -> loc T.
 Parameter locT : eqType.
+Variant loc : ml_type -> Type := mkloc T : locT -> loc T.
 Parameter loc_id : forall T, loc T -> locT.
 Parameter coq_type : forall M : Type -> Type, ml_type -> Type.
 End MLTY.
