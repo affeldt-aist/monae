485da734ce5cf48782dc55b40c0e11cf77c8bd49
ryuji816 <ryuji.sg100@icloud.com>
2025-03-20 22:44:30 +0900

    modify the statement of partial correctness lemma

--- theories/models/delay_assert_model.v
+++ theories/models/delay_assert_model.v
@@ -17,31 +17,44 @@ Hint Extern 0 (wBisimDE _ _) => setoid_reflexivity.
 Lemma whileDEE A B (body : A -> M (B + A)) x :
   whileDE body x = while (@DEA Delay _ _ \o body) x.
 Proof. by []. Qed.
-Check assert.
-Lemma pcorrect X x p (f : X -> M (X + X)) :
-   assert p x ≈e @ret M _ x  ->
-   (forall x, assert p x ≈e @ret M _ x ->
-          f x >>= sum_rect (fun => M X) (assert p) (assert p) ≈e f x >>= sum_rect (fun => M X) Ret Ret) ->
-   bassert p (whileDE f x) ≈e whileDE f x.
+
+Lemma assertE X x (p : pred X) : @assert M _ p x ≈e Ret x <-> (p x) = true.
+Proof.
+split.
+  rewrite/assert.
+  case: (p x) => //.
+  rewrite guardF bindfailf /wBisimDE.
+  move/iff_wBisims_wBisim.
+  move/iff_Terminates_wBret => contr.
+  inversion contr.
+rewrite/assert => ->.
+by rewrite guardT bindskipf.
+Qed.
+Lemma pcorrect X A x (p : pred (A + X)) (f : X -> M (A + X)) :
+   p (inr x)  ->
+   (forall x, p (inr x) ->
+          f x >>= sum_rect (fun => M (A + X)) ((assert p) \o inl) ((assert p) \o inr) ≈e
+    f x >>= sum_rect (fun => M (A + X)) (Ret \o inl) (Ret \o inr)) ->
+   bassert p ((whileDE f x) >>= (Ret \o inl)) ≈e whileDE f x >>= (Ret \o inl).
 Proof.
 move => Hx HInv.
 case: (TerminatesP (whileDE f x)) =>
   [[[u' /iff_Terminates_wBret/iff_wBisims_wBisim Hs
     |x' /iff_Terminates_wBret [n Hs]]]|/iff_Diverges_wBisimspin Hs].
-- by rewrite/bassert bindXE Hs bindretf.
+- by rewrite/bassert bindXE Hs !bindretf bindXE !bindretf.
 - rewrite steps_Dnow in Hs.
   move: x x' Hx Hs.
   elim: n => [/=|n IH] x x' Hx;
              rewrite whileDEE whileE /DEA functions.compE fmapE.
-    case Hb: (f x) => [uxx|].
+    case Hb: (f x) => [uxx|d].
       case: uxx Hb => [u//|[y/=|y/=]] Hb;
                       rewrite bindretf/=bindretf.
       - by rewrite/bassert bindXE bindretf.
       - rewrite/bassert bindXE bindretf => _.
         move: (HInv x Hx).
-        by rewrite Hb !bindretf /=.
+        by rewrite Hb !bindretf/retX /=.
       - by rewrite /retX => contr.
-    by rewrite !bindDmf/retX => contr.
+    by rewrite !bindDmf => contr.
   set d := f x.
   have : d ≈ f x by [].
   move: d.
@@ -54,20 +67,30 @@ case: (TerminatesP (whileDE f x)) =>
       by rewrite -Hb !bindretf /=.
     move: (HInv x Hx).
     rewrite !bindXE -Hb !bindretf/=.
+    move => HH.
     move/IH => IH'.
-    move/IH'.
-    by rewrite {1}whileDEE /DEA /bassert !bindXE
-         !bindDmf -!bindXE !wBisim_DLater.
-  move => Hd' HH.
-  rewrite/bassert bindXE !bindA !bindDmf.
-  apply: wBLater.
+    rewrite -!whileDEE.
+    rewrite /bassert !bindDmf.
+    rewrite wBisim_DLater -bindXE.
+    rewrite -{2}IH' /bassert.
+    by rewrite /retX.
+    move: HH.
+    by move/assertE.
+    rewrite/bassert !bindXE !bindA !bindDmf /= => Hd' Hs.
+    apply wBLater.
   rewrite -!bindA -bindXE -/(bassert p _).
   apply: CIH.
-    by rewrite -Hd' wBisim_DLater.
+  by rewrite -Hd' wBisim_DLater.
   apply: monotonicity_steps'.
-  by rewrite -bindDmf -bindDmf.
-rewrite/bassert bindXE Hs.
-by apply/iff_Diverges_wBisimspin/Diverges_bindspinf.
+  by rewrite /= /bassert bindA Hs.
+rewrite !bindXE /bassert Hs.
+apply: wBisim_trans.
+apply iff_Diverges_wBisimspin.
+  rewrite bindA.
+  by apply: Diverges_bindspinf.
+apply: wBisim_sym.
+apply iff_Diverges_wBisimspin.
+by apply Diverges_bindspinf.
 Qed.
 
 HB.instance Definition _ := @isMonadDelayAssert.Build M (@pcorrect).
