114bf0d174b94b5422266e637845c581e8543e63
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-06-10 22:42:28 +0900

    cleaning

--- theories/applications/example_delayassert.v
+++ theories/applications/example_delayassert.v
@@ -3,6 +3,14 @@
 From mathcomp Require Import all_ssreflect.
 Require Import hierarchy.
 
+(**md**************************************************************************)
+(* # Applications of the Delay-assert monad                                   *)
+(*                                                                            *)
+(* ```                                                                        *)
+(*   bubblesort == TODO                                                       *)
+(* ```                                                                        *)
+(******************************************************************************)
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -14,60 +22,53 @@ Section bubblesort.
 Variable M : delayAssertMonad.
 
 Fixpoint sortl (l : seq nat) :=
-  match l with
-  | nil => nil
-  | n :: tl => match tl with
-              | nil => l
-              | m :: tl' => if m < n then m :: n :: sortl tl'
-                                    else n :: sortl tl
-              end
-  end.
+  if l is n :: tl then
+    (if tl is m :: tl' then
+       (if m < n then
+          m :: n :: sortl tl'
+        else n :: sortl tl)
+     else l)
+  else nil.
 
 Definition bubblesort_body (l : seq nat) : M (seq nat + seq nat)%type :=
   if l == sortl l then Ret (inl l) else Ret (inr (sortl l)).
 
 Definition bubblesort l := while bubblesort_body l.
 
-Definition sizelE (l : seq nat) : pred (seq nat) := fun l' => size l == size l'.
-
-Lemma sizeE (n : nat) l : size (n :: l) = (size l).+1.
-Proof. by []. Qed.
-
-Lemma sortl_length (l : seq nat) : size l = size (sortl l).
+Lemma sortl_length l : size l = size (sortl l).
 Proof.
 move Hlen: (size l) => n.
 elim: n {-2}n (leqnn n) l Hlen => n.
-  rewrite leqn0 => /eqP H0 l.
-  rewrite H0.
-  move/size0nil => -> //.
-move => IH m Hmn l.
-case: l => [|h tl] //=.
-case: tl => [|k tl] // Hs.
-have [Hhk|Hkh] := (leqP h k).
-  rewrite sizeE -Hs.
-  apply: eq_S.
+  rewrite leqn0 => /eqP -> l.
+  by move/size0nil => ->.
+move=> IH m Hmn.
+move=> [|h] //=.
+move=> [|k tl] // Hs.
+have [hk|kh] := leqP h k.
+  rewrite [k :: tl]lock/= -lock -Hs.
+  congr S.
   rewrite -(IH (size (k :: tl))) //.
-    by rewrite -(leq_add2r 1 _ _) !addn1 Hs.
-rewrite -Hs !sizeE -(IH (size tl)) //.
-move: Hmn.
-rewrite -Hs -addn1 -(addn1 n) (leq_add2r 1 _ _)/=.
-exact: ltnW.
+  by rewrite -(leq_add2r 1 _ _) !addn1 Hs.
+rewrite -Hs [k :: tl]lock/= -lock -(IH (size tl)) //.
+by move: Hmn; rewrite -Hs/= ltnS; exact: ltnW.
 Qed.
+
+Definition sizelE (l : seq nat) : pred (seq nat) := fun l' => size l == size l'.
 
 Theorem bubblesort_length l : bassert (sizelE l) (bubblesort l) ≈ bubblesort l.
 Proof.
 apply: pcorrect.
   by rewrite assertE /sizelE eq_refl guardT bindskipf.
-move => l' Inv.
-rewrite/bubblesort_body.
+move=> l' Inv.
+rewrite /bubblesort_body.
 case: ifP => /eqP H.
-  by rewrite bindretf/= bindretf/=.
+  by rewrite bindretf/= bindretf.
 move: Inv.
-rewrite !bindretf/= !assertE !/sizelE (sortl_length l') /guard.
-case: ifP => _.
+rewrite !bindretf/= !assertE !/sizelE (sortl_length l').
+rewrite /guard; case: ifP => _.
   by rewrite !bindskipf.
 rewrite !bindfailf => contr.
-pose f := fun (_ :seq nat) => @ret M _ (sortl l').
+pose f := fun (_ : seq nat) => @ret M _ (sortl l').
 by rewrite -(bindfailf _ _ f) contr bindretf.
 Qed.
 
--- theories/applications/example_delayexcept.v
+++ theories/applications/example_delayexcept.v
@@ -4,6 +4,11 @@ From mathcomp Require Import all_ssreflect.
 From mathcomp Require Import boolp.
 Require Import preamble hierarchy monad_lib fail_lib state_lib.
 
+(**md**************************************************************************)
+(* # Applications of the Delay-exception monad                                *)
+(*                                                                            *)
+(******************************************************************************)
+
 Local Open Scope monae_scope.
 
 Section select.
@@ -13,10 +18,13 @@ Definition select l := let max := \max_(i <- l) i in (max, rem max l).
 
 Definition muloflistover10d_body ln : M (nat + (seq nat) * nat)%type :=
   match ln with
-    (l, n) => match l with
-              | [::] => Ret (inl n)
-              | h::tl => let (m, res) := select l in catch (if m <= 10 then fail else Ret (inr (res, m * n))) (Ret (inl n))
-              end
+    (l, n) => if l is h::tl then
+                let (m, res) := select l in
+                catch (if m <= 10 then
+                         fail
+                       else Ret (inr (res, m * n))) (Ret (inl n))
+              else
+                Ret (inl n)
   end.
 
 Definition muloflistover10d := while muloflistover10d_body.
@@ -26,35 +34,28 @@ Definition muloflistover10 l := foldr (fun x z => if 10 < x then x * z else z) 1
 Lemma all_under10 l : (forall i, i \in l -> i <= 10) -> muloflistover10 l = 1.
 Proof.
 elim: l => //= n l' Hl H.
-have [H10|_] := ltnP 10 n.
-  contradict H10.
-  apply/negP.
-  rewrite -leqNgt.
-  apply H.
-  by rewrite in_cons eq_refl.
-apply Hl => m Hl'.
-apply H.
-by rewrite in_cons Hl' orbT.
+have [n10|_] := ltnP 10 n.
+  contradict n10.
+  by apply/negP; by rewrite -leqNgt H// mem_head.
+rewrite Hl// => m Hl'.
+by rewrite H// inE Hl' orbT.
 Qed.
 
-Lemma muloflistover10_aux(l : seq nat) (k : nat): 10 < k -> k \in l -> k * muloflistover10 (rem k l) = muloflistover10 l.
+Lemma muloflistover10_aux(l : seq nat) (k : nat) : 10 < k -> k \in l ->
+  k * muloflistover10 (rem k l) = muloflistover10 l.
 Proof.
-move => Hk Hink.
-rewrite/muloflistover10.
-elim: l Hink => //= n l' IH Hink.
-have  [Hnk|/negPf Hnk] := eqVneq n k.
-  have [|?] := ltnP 10 n.
-    by rewrite Hnk.
+move=> Hk Hink.
+rewrite /muloflistover10.
+elim: l => //= n l' IH in Hink *.
+have [nk|nk] := eqVneq n k.
+  have [|?] := ltnP 10 n; first by rewrite nk.
   contradict Hk.
-  rewrite -Hnk.
-  apply/negP.
-  by rewrite -ltnNge.
+  rewrite -nk.
+  by apply/negP; rewrite -ltnNge.
 have Hkl': k \in l'.
-  move: Hink.
-  by rewrite in_cons eq_sym Hnk/=.
+  by move: Hink; rewrite in_cons eq_sym (negbTE nk).
 have [Hn|HnN] := ltnP 10 n.
-  rewrite -(IH Hkl')/= Hn.
-  by rewrite mulnA (mulnC k n) -mulnA.
+  by rewrite -(IH Hkl')/= Hn mulnA (mulnC k n) -mulnA.
 by rewrite -(IH Hkl')/= ltnNge HnN/=.
 Qed.
 
@@ -62,55 +63,51 @@ Lemma maxinseq (l : seq nat) : ~~ nilp l -> \max_(i <- l) i \in l.
 Proof.
 move => Hn.
 rewrite -(in_tupleE l) big_tuple.
-case: (eq_bigmax (tnth (in_tuple l))) => [|x ->].
+have [|x ->] := eq_bigmax (tnth (in_tuple l)).
   by rewrite cardT size_enum_ord lt0n.
 by rewrite mem_tnth.
 Qed.
 
-Lemma muloflistover10E (l : seq nat) (n : nat) : muloflistover10d (l, n) ≈ Ret (n * (muloflistover10 l)).
+Lemma muloflistover10E (l : seq nat) (n : nat) :
+  muloflistover10d (l, n) ≈ Ret (n * (muloflistover10 l)).
 Proof.
 move Hlen: (size l) => len.
 move: n.
 elim: len l Hlen.
-- move => l /eqP/nilP Hl n.
-  by rewrite/muloflistover10/muloflistover10d/muloflistover10d_body Hl /= fixpointE /= bindretf /= mulnS muln0 addn0.
-move => m IH l' Hs n.
+  move => l /eqP/nilP Hl n.
+  rewrite/muloflistover10/muloflistover10d/muloflistover10d_body Hl /=.
+  by rewrite fixpointE /= bindretf /= mulnS muln0 addn0.
+move=> m IH l' Hs n.
 rewrite/muloflistover10d/muloflistover10d_body fixpointE /=.
-elim: l' Hs => //= [h l''] _ Hs.
-case: Hs => Hs.
-have [/bigmax_leqP_seq Hm10|Hm10] := leqP (\max_(i <- (h :: l'')) i) 10.
+elim: l' Hs => //= [h l''] _ []Hs.
+have [/bigmax_leqP_seq Hm10|Hm10] := leqP (\max_(i <- h :: l'') i) 10.
   rewrite catchfailm bindretf /=.
-  have -> : (10 < h) = false.
-    apply/negP/negP.
-    rewrite -leqNgt.
-    apply Hm10 => //=.
-    by rewrite in_cons eq_refl.
+  rewrite ifF; last first.
+    apply/negP/negP; rewrite -leqNgt.
+    by rewrite Hm10//= mem_head.
   rewrite all_under10.
-    by rewrite mulnS muln0 addn0.
-  move => i Hini.
-  apply Hm10 => //=.
-  by rewrite in_cons Hini orbT.
+    by rewrite muln1.
+  move=> i Hini.
+  by rewrite Hm10//= in_cons Hini orbT.
 rewrite catchret bindretf /=.
 rewrite -/muloflistover10d_body -/muloflistover10d.
-have [Ht|Hf] := eqVneq h (\max_(i <- (h :: l'')) i).
+have [Ht|Hf] := eqVneq h (\max_(i <- h :: l'') i).
   by rewrite {2}Ht (ifT _ _ Hm10) (IH l'' Hs) -Ht (mulnC h n) mulnA.
 move: Hm10.
-set k := \max_(i <- (h :: l'')) i => Hk.
-have Hmaxin : k \in (h :: l'').
-  subst k.
-  by apply maxinseq.
+set k := \max_(i <- h :: l'') i => k10.
+have Hmaxin : k \in (h :: l'') by rewrite maxinseq.
 rewrite IH/=.
   rewrite /= (mulnC k n) -mulnA fun_if.
-  rewrite (mulnA k h _) (mulnC k h) -mulnA (muloflistover10_aux _ _ Hk) //.
-  by move: Hmaxin; rewrite in_cons eq_sym (negPf Hf) /=.
+  rewrite (mulnA k h _) (mulnC k h) -mulnA (muloflistover10_aux _ _ k10) //.
+  by move: Hmaxin; rewrite in_cons eq_sym (negPf Hf).
 subst k.
-rewrite  size_rem.
-  case: l'' Hs Hf Hk Hmaxin => [? contr|h' l'''  Hs ? ?].
+rewrite size_rem.
+  case: l'' Hs Hf k10 Hmaxin => [? contr|h' l'''  Hs ? ?].
     contradict contr.
     by rewrite big_cons big_nil maxn0 eq_refl.
-  by rewrite prednK //=.
+  by rewrite prednK.
 move: Hmaxin.
-by rewrite in_cons eq_sym (negPf Hf) /=.
+by rewrite in_cons eq_sym (negPf Hf).
 Qed.
 
 End select.
